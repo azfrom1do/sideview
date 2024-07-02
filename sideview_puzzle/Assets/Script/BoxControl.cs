58b2417fb6f8191d2632f19ab0d2a92c2c5faf29
azfrom1do <128368827+azfrom1do@users.noreply.github.com>
2024-07-02 16:46:14 +0900

    PlayerMime
    BoxControl

--- sideview_puzzle/Assets/Script/BoxControl.cs
+++ sideview_puzzle/Assets/Script/BoxControl.cs
@@ -6,14 +6,47 @@ using UnityEngine;
 public class BoxControl : MonoBehaviour
 {
     private Rigidbody rigidbody;
+    GameObject player;
+    Rigidbody playerRigid;
+    float boxSizeX;
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            playerRigid = player.GetComponent<Rigidbody>();
+        }
+        else Debug.Log("Player Tag not found");
+
+        boxSizeX = GetComponent<BoxCollider>().size.x * transform.localScale.x;
+        //박스의 x길이
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //PlayerMime();
+
+    }
+
+    //플레이어와의 상호작용
+    private void PlayerMime()
+    {
+        float distance = Vector3.Distance(playerRigid.position, rigidbody.position);
+        //플레이어와의 거리
+
+        if (distance < boxSizeX * 0.5f + 0.8f && player.gameObject.GetComponent<PlayerMove>().canjump)
+        {
+            if (playerRigid.position.x > rigidbody.position.x)
+            {
+                //플레이어가 박스의 오른쪽에서 밀때 왼쪽으로 이동
+                rigidbody.transform.Translate(Vector3.left * player.gameObject.GetComponent<PlayerMove>().speed * Time.deltaTime);
+            }
+            if (playerRigid.position.x < rigidbody.position.x)
+            {
+                //플레이어가 박스의 왼쪽에서 밀때 오른쪽으로 이동
+                rigidbody.transform.Translate(Vector3.right * player.gameObject.GetComponent<PlayerMove>().speed * Time.deltaTime);
+            }
+        }
     }
 }
