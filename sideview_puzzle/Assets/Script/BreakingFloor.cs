b4fa2d663315c346f07acc536c66f9bdf726cf05
azfrom1do <128368827+azfrom1do@users.noreply.github.com>
2024-07-09 18:10:30 +0900

    24.07.09 Floor Falling, 5F Prefab
    무너지는 바닥 수정, 5층

--- sideview_puzzle/Assets/Script/BreakingFloor.cs
+++ sideview_puzzle/Assets/Script/BreakingFloor.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))] // Add Rigidbody Component
 public class BreakingFloor : MonoBehaviour
 {
-    public Color ColorToChange = new Color(255 / 255f, 127 / 255f, 127 / 255f); //default is light red
+    public Color ColorToChange = new Color(255 / 255f, 127 / 255f, 127 / 255f, 127 / 255f); //default is light red
     public float breakingTime = 1f;
     public float regenTime = 3f;
 
     private Vector3 originPoint;
     private Color originColor;
+    private bool isFalling; //BreakingFloor끼리 닿으면 소멸해버서 조건 추가
 
     /* 생성시 현재 위치와 현재 자신의 색상을 기억
      * 플레이어가 위에서 닿으면 Breaking 코루틴 호출
@@ -28,13 +29,18 @@ public class BreakingFloor : MonoBehaviour
         gameObject.GetComponent<Rigidbody>().isKinematic = true;
         gameObject.GetComponent<Rigidbody>().constraints = 
             RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+        isFalling = false;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag.Equals("Player") && collision.transform.position.y > transform.position.y)
             StartCoroutine(Breaking());
-        if (collision.gameObject.tag.Equals("Floor"))
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Floor") && isFalling)
         {
             gameObject.GetComponent<Renderer>().enabled = false;
             gameObject.GetComponent<Collider>().enabled = false;
@@ -53,20 +59,24 @@ public class BreakingFloor : MonoBehaviour
             transform.position += Vector3.left * 0.04f;
         }
 
+        gameObject.GetComponent<Collider>().isTrigger = true;
         gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        isFalling = true;
 
         yield return new WaitForSeconds(regenTime);
 
         gameObject.GetComponent<Renderer>().enabled = true;
         gameObject.GetComponent<Collider>().enabled = true;
+        gameObject.GetComponent<Collider>().isTrigger = false;
         gameObject.GetComponent<Rigidbody>().isKinematic = true;
         transform.position = originPoint;
         gameObject.GetComponent<Renderer>().material.color = originColor;
+        isFalling = false;
     }
 }
 
 /* 
  * 최초작성 : 2024.07.08
- * 변경일자 : 2024.07.08
+ * 변경일자 : 2024.07.09
  * 작업자 : 윤종현
  */
