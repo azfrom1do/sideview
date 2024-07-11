94ab1b1aec037ba64adc369e396d208396326149
detect564l <detect5641@gmail.com>
2024-07-11 19:05:42 +0900

    7.11 추가 수정
    리셋 시스템 완성

--- sideview_puzzle/Assets/Script/DeathSystem.cs
+++ sideview_puzzle/Assets/Script/DeathSystem.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class DeathSystem : MonoBehaviour
 {
     private Transform playerTransform;
+    public GameObject currentMap;
+    public GameObject Frefab1F;
+    public GameObject Frefab2F;
+    public GameObject Frefab3F;
+    public GameObject Frefab4F;
+    public GameObject Frefab5F;
+    public Transform mapPosition;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -22,8 +30,46 @@ public class DeathSystem : MonoBehaviour
     }
     public void Death()
     {
-        if( 8 < PlayerPrefs.GetInt("PointNum")&& PlayerPrefs.GetInt("PointNum")< 11)
+        if (0 < PlayerPrefs.GetInt("PointNum") && PlayerPrefs.GetInt("PointNum") <= 4)
         {
+            currentMap = GameObject.FindWithTag("1F");
+            Transform mapTransform = currentMap.transform;
+            Destroy(currentMap.gameObject);
+            currentMap = Instantiate(Frefab1F, mapTransform.position, mapTransform.rotation);
+            character();
+        }
+        if (4 < PlayerPrefs.GetInt("PointNum") && PlayerPrefs.GetInt("PointNum") <= 7)
+        {
+            currentMap = GameObject.FindWithTag("2F");
+            Transform mapTransform = currentMap.transform;
+            Destroy(currentMap.gameObject);
+            currentMap = Instantiate(Frefab2F, mapTransform.position, mapTransform.rotation);
+            character();
+        }
+        if (7 < PlayerPrefs.GetInt("PointNum") && PlayerPrefs.GetInt("PointNum") <= 9)
+        {
+            currentMap = GameObject.FindWithTag("3F");
+            Transform mapTransform = currentMap.transform;
+            Destroy(currentMap.gameObject);
+            currentMap = Instantiate(Frefab3F, mapTransform.position, mapTransform.rotation);
+            character();
+        }
+
+        if (9 < PlayerPrefs.GetInt("PointNum")&& PlayerPrefs.GetInt("PointNum")<=11)
+        {
+            currentMap = GameObject.FindWithTag("4F");
+            Transform mapTransform = currentMap.transform;
+            Destroy(currentMap.gameObject);
+            currentMap = Instantiate(Frefab4F, mapTransform.position, mapTransform.rotation);
+            character();
+        }
+
+        if (11 < PlayerPrefs.GetInt("PointNum") && PlayerPrefs.GetInt("PointNum") <= 14)
+        {
+            currentMap = GameObject.FindWithTag("5F");
+            Transform mapTransform = currentMap.transform;
+            Destroy(currentMap.gameObject);
+            currentMap = Instantiate(Frefab5F, mapTransform.position, mapTransform.rotation);
             character();
         }
     }
@@ -32,6 +78,5 @@ public class DeathSystem : MonoBehaviour
         playerTransform.position = new Vector3(PlayerPrefs.GetFloat("PlayerPositionX"),
                         PlayerPrefs.GetFloat("PlayerPositionY"),
                         PlayerPrefs.GetFloat("PlayerPositionZ"));
-        
     }
 }
